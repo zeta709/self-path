a08dda12ac8e00d4296a0557ea4152e8a34d702b
zeta709 <zeta709@gmail.com>
2018-03-03 12:38:45 +0900

    Add description

--- self_path.zsh
+++ self_path.zsh
@@ -1,5 +1,13 @@
 #!/bin/zsh
+
 SELF_DIR="${${(%):-%x}:A:h}"
 # https://stackoverflow.com/questions/9901210/bash-source0-equivalent-in-zsh
-# In the comment of the above page, `${(%):-%x}` is suggested,
-# which is the name of the current file.
+#     In the comment of the above page, `${(%):-%x}` is suggested,
+#     which is the name of the current file.
+# See also:
+#     man zshexpn
+#         Parameter Expansion
+#         Modifiers
+#     man zshmisc
+#         Simple Prompt Escapes
+# TODO: What does "P" modifier diffent from "A" modifier?
